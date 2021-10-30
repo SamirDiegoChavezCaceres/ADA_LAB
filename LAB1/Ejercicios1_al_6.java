e49bcfe8fdff9aa08e960f92c5fc8d53e76858be
SamirDiegoChavezCaceres <schavezca@unsa.edu.pe>
2021-10-30 15:18:06 -0500

    Se sube lo avanzado en laboratorio5

--- LAB1/Ejercicios1_al_6.java
+++ LAB1/Ejercicios1_al_6.java
@@ -6,7 +6,24 @@
 package LAB1;
 
 /**
- *
+  Review
+for (i = 0; i < n; i++)       // O(n)
+
+
+for (i = 0; i < n; i=i+2)     // O(n)
+
+
+for (i = n; i >= 1; i--)      // O(n)
+
+
+for (i = 1; i < n; i=i*2)     // O(log_2(n))
+
+
+for (i = 1; i < n; i=i*3)     // O(log_3(n))
+
+
+for (i = n; i >= 1; i=i/2)    // O(log_2(n))
+
  * @author Usuario
  */
 public class Ejercicios1_al_6 {
