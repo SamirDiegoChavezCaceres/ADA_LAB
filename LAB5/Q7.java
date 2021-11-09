535faf89ef3110e80e6273e561afbcfbdd15984b
SamirDiegoChavezCaceres <schavezca@unsa.edu.pe>
2021-11-09 17:56:19 -0500

    agregando main

--- LAB5/Q7.java
+++ LAB5/Q7.java
@@ -1,7 +1,13 @@
+//Revisando tiempo computacional
+//Q7
+public static void main(String[] args) {
+	q7();
+}
 public static void q7() {	
-	for (i = 1; i < n; i = i*2) {
+	int n = 10;
+	for (int i = 1; i < n; i = i*2) {
 		System.out.println(i+" Log");
 	}
 }
-Seria 2^k en caso de que afectara n pero como afecta a i ... 2^k = n 
-k = log2(n) = O(logn)
+//Seria 2^k en caso de que afectara n pero como afecta a i ... 2^k = n 
+//k = log2(n) = O(logn)
