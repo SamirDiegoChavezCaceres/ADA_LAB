5901c6c8c4820d5f123acd86d46b493a2a11d606
SamirDiegoChavezCaceres <schavezca@unsa.edu.pe>
2021-11-08 19:11:47 -0500

    Create ejercicio3.java

---
+++ LAB4/ejercicio3.java
@@ -0,0 +1,15 @@
+public class ejercicios {
+    public static void main(String[] args) {
+        int[]array = {4,5,6,2,7,8,9};
+    }
+  
+    //ejercicio3
+    //Dado un arreglo busca dos numeros diferentes indices que sumados den cierto numero en este caso 10
+    public static boolean twoSum(int[] array){ //----- >  O(n^2)al tenes un for dentro de otro for
+        for (int i = 0; i < array.length ; i++)
+            for (int j = 0; j < array.length ; j++)// ----- > n veces
+                if (i != j && array[j] + array[i] == 10)
+                    return true;
+        return false;
+    }
+}
