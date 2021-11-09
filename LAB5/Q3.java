909d3355d5106e50c3def4345e05716eba59af13
SamirDiegoChavezCaceres <schavezca@unsa.edu.pe>
2021-11-09 17:53:42 -0500

    Agregando main

--- LAB5/Q3.java
+++ LAB5/Q3.java
@@ -1,7 +1,14 @@
-public static void q3() {	
-	for (i = 0; i < n; i=i+5) {
+//Revisando tiempo computacional
+//Q2
+public static void main(String[] args) {
+	q3();
+}
+public static void q3() {
+	int  n = 0;
+	
+	for (int i = 0; i < n; i=i+5) {
 		System.out.println(i+" Lineal");
 	}
 }
 //Al ser solo una constante n/2 no afectaria a la notacion O(n)
-0 2 4 6 ... n - 2
+//0 2 4 6 ... n - 2
