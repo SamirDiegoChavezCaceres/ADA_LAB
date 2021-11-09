53df58c9bb67eb94de2cc71b82de911251537eb4
SamirDiegoChavezCaceres <schavezca@unsa.edu.pe>
2021-11-09 17:17:39 -0500

    agregue main

--- LAB5/Q11.java
+++ LAB5/Q11.java
@@ -1,12 +1,16 @@
+public static void main(String[] args) {
+	q11();
+    }
 public static void q11() {	
-	p = 0
-	for (i = 1; i < n; i = i * 2) {
+	int n = 10;
+	int p = 0;
+	for (int i = 1; i < n; i = i * 2) {
 		p++;
 		System.out.println(i+" Log");
 	}
 	//Vimos que este era Log(n) pero la complejidad pasa a traves de esta variable p
 
-	for (j = 1; j < p; j = j * 2) {
+	for (int j = 1; j < p; j = j * 2) {
 		System.out.println(j+" Log de log");
 	}
 }
