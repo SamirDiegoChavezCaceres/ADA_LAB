03cb6fad1fa7d71f1dc316889023e749a98f79f8
SamirDiegoChavezCaceres <schavezca@unsa.edu.pe>
2021-11-09 17:24:32 -0500

    se agrega main

--- LAB5/Q12.java
+++ LAB5/Q12.java
@@ -1,8 +1,14 @@
-public static void q12() {	
-	for (i = 0; i  n; i++) {
-		for (j = 1; j  n; j = j  2) {
+//Revisando tiempo computacional
+//Q12
+public static void main(String[] args) {
+	q12();
+}
+public static void q12() {
+	int n = 10;
+	for (int i = 0; i < n; i++) {
+		for (int j = 1; j < n; j = j * 2) {
 			System.out.println(j+" n log");
 		}
 	}
 }
-el primero es n y el segundo es log n, multiplicando O(nlog(n))
+//el primero es n y el segundo es log n, multiplicando O(nlog(n))
