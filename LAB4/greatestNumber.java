89d98e17efeb0eca969ebf19d237a9f91b28a1cd
SamirDiegoChavezCaceres <schavezca@unsa.edu.pe>
2021-11-20 19:59:22 -0500

    Update and rename ejercicio1.java to greatestNumber.java

--- LAB4/greatestNumber.java
+++ LAB4/greatestNumber.java
@@ -1,12 +1,12 @@
-public class ejercicio1 {
+public class greatestNumber {
     public static void main(String[] args) {
         int[]array = {4,5,6,2,7,8,9};
-        System.out.println("El mayor de todo el arreglo es:"+greatestNumber(array));
+        System.out.println("El mayor de todo el arreglo es:"+greatestNumber1(array));
     }
     
     //ejercicio1
     //encuentra el mayor numero en un tiempo n
-    public static int greatestNumber(int[] array){
+    public static int greatestNumber1(int[] array){
         int max = array[0];
         for (int i = 1; i < array.length ; i++)
             if(max < array[i])
