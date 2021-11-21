38df1b63f450d67d972d361a7e1413bfc2c8cc4b
SamirDiegoChavezCaceres <schavezca@unsa.edu.pe>
2021-11-20 19:59:56 -0500

    Update and rename ejercicio2.java to every_other.java

--- LAB4/every_other.java
+++ LAB4/every_other.java
@@ -1,12 +1,12 @@
-public class ejercicio2 {
+public class every_other {
     public static void main(String[] args) {
         int[]array = {4,5,6,2,7,8,9};
-        every_other(array);
+        every_other1(array);
     }
     
     //ejercicio2
     //encuentra un indice par y el numero de esa posicion se imprime sumado a cada numero del arreglo
-    public static void every_other(int[] array){ //----- >  O(n^2)
+    public static void every_other1(int[] array){ //----- >  O(n^2)
         for (int i = 0; i < array.length ; i++)// O(n) * n =  ----- > O(n^2)
             if(i % 2 == 0)// O(1)*n = ----- > O(n)
                 for (int j = 0; j < array.length ; j++)// ----- > n veces
