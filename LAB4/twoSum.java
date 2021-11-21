a5e00c565349242d41e43ccc71da378bb828f220
SamirDiegoChavezCaceres <schavezca@unsa.edu.pe>
2021-11-20 20:00:37 -0500

    Update and rename ejercicio3.java to twoSum.java

--- LAB4/twoSum.java
+++ LAB4/twoSum.java
@@ -1,12 +1,12 @@
-public class ejercicio3 {
+public class twoSum {
     public static void main(String[] args) {
         int[]array = {4,5,6,2,7,8,9};
-        System.out.println(twoSum(array));
+        System.out.println(twoSum1(array));
     }
   
     //ejercicio3
     //Dado un arreglo busca dos numeros diferentes indices que sumados den cierto numero en este caso 10
-    public static boolean twoSum(int[] array){ //----- >  O(n^2)al tenes un for dentro de otro for
+    public static boolean twoSum1(int[] array){ //----- >  O(n^2)al tenes un for dentro de otro for
         for (int i = 0; i < array.length ; i++)
             for (int j = 0; j < array.length ; j++)// ----- > n veces
                 if (i != j && array[j] + array[i] == 10)
