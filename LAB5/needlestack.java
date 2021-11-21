76b7b4de9bd3a29dde22f3bbcc937c007aad986a
SamirDiegoChavezCaceres <schavezca@unsa.edu.pe>
2021-11-20 23:23:10 -0500

    Se cambia nombre al archivo y se arreglan errores de compilacion

--- LAB5/needlestack.java
+++ LAB5/needlestack.java
@@ -3,26 +3,31 @@
 package LAB5;
 public class needlestack {
 public static void main(String[] args) {
-	find_needle("cadabra","abracadabra");
+	System.out.println(find_needle("cadabra","abracadabra"));
+        System.out.println("Bye");
 }
 //pick_neddle esta en el lab anterior se copia lo que envie
 //Dado dos strings busca si needle esta dentro de la cadena haystack
     public static boolean find_needle(String needle, String haystack){ //O(n*m) donde n es la longitud de needle y
         int needle_index = 0;                                           // m la longitud de haystack
         int haystack_index = 0;
-        boolean foundneedle = false;
+        boolean foundneedle;
         
         while(haystack_index < haystack.length()){
             if(needle.charAt(needle_index) == haystack.charAt(haystack_index)){
                 foundneedle = true;
-                while(needle_index < needle.length())
-                    if(needle.charAt(needle_index) != haystack.charAt(haystack_index))
+                int aux = haystack_index;
+                while(needle_index < needle.length() && foundneedle){
+                    if(needle.charAt(needle_index) != haystack.charAt(aux) && foundneedle)
                         foundneedle = false;
-            }
-            if(foundneedle)
-                return true;
-            needle_index = 0;
-            haystack_index += 1;
+                    aux++;
+                    needle_index++;
+                }
+                needle_index = 0;
+                if(foundneedle)
+                    return true;
+            } 
+            haystack_index++;
         }
         return false;
     }
