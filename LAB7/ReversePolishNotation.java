5b9c18c424ae81d50631f7625e8b4156a3d517ef
SamirDiegoChavezCaceres <schavezca@unsa.edu.pe>
2021-11-21 20:03:11 -0500

    Se añade la resolucion a notacion polaca

---
+++ LAB7/ReversePolishNotation.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LAB7;
+
+/**
+ *
+ * autor: Chavez Caceres Samir 
+ * ejercicio: Evaluate Reverse Polish Notation
+ * proposito: evalua la notacion polaca y da el resultado
+ *            
+ */
+public class ReversePolishNotation {
+    public static void main(String[] args) throws ExceptionIsEmpty {
+        String[]str = {"10","6","9","3","+","-11","*","/","*","17","+","5","+",};
+        System.out.println(reversePolishNotation(str));
+    }
+
+    public static String reversePolishNotation(String[] str) throws ExceptionIsEmpty {
+        StackLink<String> stack = new StackLink<String>();
+        int result, op1, op2, flag;
+        result = 0;
+        for (String str1 : str) {
+            flag = -1;
+            if (str1.equals("+")){
+                flag = 0;
+            }
+            if (str1.equals("-")){
+                flag = 1;
+            }
+            if (str1.equals("/")){
+                flag = 2;
+            }
+            if (str1.equals("*")){
+                flag = 3;
+            }
+            if(flag != -1) {
+                
+                op1 = Integer.parseInt(stack.pop());
+                op2 = Integer.parseInt(stack.pop());
+                if(flag == 0)
+                    result = op2 + op1;
+                if(flag == 1)
+                    result = op2 - op1;
+                if(flag == 2)
+                    result = op2 / op1;
+                if(flag == 3)
+                    result = op2 * op1;
+                stack.push(result+"");
+            } else
+                stack.push(str1);
+        }
+        return stack.pop();
+    }
+    
+}
