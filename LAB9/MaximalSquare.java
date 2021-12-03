b8496abe08c910b64dff2b88539ab494a205ca19
SamirDiegoChavezCaceres <schavezca@unsa.edu.pe>
2021-12-03 10:33:46 -0500

    Solucion al ejercicio 5 Maximal Square

--- LAB9/MaximalSquare.java
+++ LAB9/MaximalSquare.java
@@ -8,8 +8,30 @@ package LAB9;
 /**
  * autor: Chavez Caceres Samir 
  * ejercicio: MaximalSquare
- * proposito: 
+ * proposito: Se busca el mayor cuadrado posible dado una matriz
  */
 public class MaximalSquare {
-    
+    public static void main(String[] args) {
+        int[][] mtr = {{0, 1, 1, 1, 0},
+                        {1, 1, 1, 1, 1},
+                        {0, 1, 1, 1, 1},
+                        {0, 1, 1, 1, 1},
+                        {0, 0, 1, 1, 1}};
+        System.out.println(maxSquare(mtr));
+    }
+    public static int maxSquare(int[][]matr){
+        int[][] memo = new int[matr.length + 1][matr[0].length + 1];
+        int max = 0;
+        for (int i = 0; i < matr.length; i++) {
+            for (int j = 0; j < matr[0].length; j++) {
+                if (matr[i][j] == 1){       //Como en el problema de pathfind se usa soluciones anteriores para hallar la 
+                                            //solucion y se escribe en memo.
+                    memo[i+1][j+1] = Math.min(Math.min(memo[i+1][j], memo[i][j+1]), memo[i+1][j]) + 1; 
+                    max = Math.max(max, memo[i][j]);
+                                            //el maximo se almacena en una variable
+                }
+            }
+        }
+        return (int) Math.pow(max, 2);
+    }
 }
